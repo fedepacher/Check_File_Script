f113f007ab7df9fe7e0a38b48b81eef71ce9888a
fedepacher <fede_pacher@hotmail.com>
2021-11-19 10:10:44 -0300

    Commit prueba de firma probando si sale firmado o no

--- check_files.py
+++ check_files.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is used to check that all installation files (except ignored and exceptions) have the expected permissions, owner, group ...
 # It is a free software; you can redistribute it and/or modify it under the terms of GPLv2
@@ -16,72 +14,82 @@ from collections import Counter
 
 
 _filemode_list = [
-    {stat.S_IFLNK : "l",
-    stat.S_IFREG: "-",
-    stat.S_IFBLK: "b",
-    stat.S_IFDIR: "d",
-    stat.S_IFCHR: "c",
-    stat.S_IFIFO: "p"},
-
-    {stat.S_IRUSR: "r"},
-
-    {stat.S_IWUSR: "w"},
-
-    {stat.S_IXUSR | stat.S_ISUID: "s",
-    stat.S_ISUID: "S",
-    stat.S_IXUSR: "x"},
-
-    {stat.S_IRGRP: "r"},
-
-    {stat.S_IWGRP: "w"},
-
-    {stat.S_IXGRP | stat.S_ISGID: "s",
-    stat.S_ISGID: "S",
-    stat.S_IXGRP: "x"},
-
-    {stat.S_IROTH: "r"},
-
-    {stat.S_IWOTH: "w"},
-
-    {stat.S_IXOTH | stat.S_ISVTX: "t",
-    stat.S_ISVTX: "T",
-    stat.S_IXOTH: "x"}
+    {
+        stat.S_IFLNK : "l",
+        stat.S_IFREG: "-",
+        stat.S_IFBLK: "b",
+        stat.S_IFDIR: "d",
+        stat.S_IFCHR: "c",
+        stat.S_IFIFO: "p"
+    },
+    {
+        stat.S_IRUSR: "r"
+    },
+    {
+        stat.S_IWUSR: "w"
+    },
+    {
+        stat.S_IXUSR | stat.S_ISUID: "s",
+        stat.S_ISUID: "S",
+        stat.S_IXUSR: "x"
+    },
+    {
+        stat.S_IRGRP: "r"
+    },
+    {
+        stat.S_IWGRP: "w"
+    },
+    {
+        stat.S_IXGRP | stat.S_ISGID: "s",
+        stat.S_ISGID: "S",
+        stat.S_IXGRP: "x"
+    },
+    {
+        stat.S_IROTH: "r"
+    },
+    {
+        stat.S_IWOTH: "w"
+    },
+    {
+        stat.S_IXOTH | stat.S_ISVTX: "t",
+        stat.S_ISVTX: "T",
+        stat.S_IXOTH: "x"
+    }
 ]
-# ---------------------------------------------------------------------------------------------------------------
 
-# Aux functions
-
-# ---------------------------------------------------------------------------------------------------------------
-
-"""
-    Convert a file's mode to a string of the form '-rwxrwxrwx'.
-    Parameters:
-        - mode: st_mode field of a file or directory from os.stat_result (Example: 16893)
-    Return:
-        String of the permissions set '-rwxrwxrwx'
-    Example:
-        33204 --> -rw-rw-r--
-"""
 
 def get_filemode(mode):
-    permission = []
-    for items in _filemode_list:                                        #walk through the list of dict 
-        for k,v in items.items():                                       
-            if mode & k == k:                                           #and function betwen the mode and the key of the dict
-                permission.append(v)                                    #store the value of the dict
+    """Convert a file's mode to a string of the form '-rwxrwxrwx'.
+
+    Args:
+       mode (int): st_mode field of a file or directory from os.stat_result (Example: 16893)
+
+    Return:
+        string: Permissions set '-rwxrwxrwx'
+
+    Example:
+        33204 --> -rw-rw-r--
+    """
+    file_permission = []
+    for items in _filemode_list:
+        for stat_flag, stat_value in items.items():
+            if mode & stat_flag == stat_flag:
+                file_permission.append(stat_value)
                 break
         else:
-            permission.append('-')                                      #add - in case no match
-    return "".join(permission)
+            file_permission.append('-')
+    return "".join(file_permission)
 
-# ---------------------------------------------------------------------------------------------------------------
 
-"""
-    Get the checkfile data from a file or directory.
-    Parameters:
-        - item: filepath or directory.
+def get_data_information(item):
+    """Get the checkfile data from a file or directory.
+
+    Args:
+        item (string): Filepath or directory.
+
     Return:
-        Dictonary with checkfile data.
+        dict: Dictionary with checkfile data.
+
     Example:
         '/var/ossec/active-response' -->
             {
@@ -91,182 +99,145 @@ def get_filemode(mode):
                 "type": "directory",
                 "user": "root"
             }
-"""
-
-def get_data_information(item):
+    """
     try:
-        stat_info = os.stat(item)                                           #get item information in a 10 lenght arrary
+        stat_info = os.stat(item)
 
-        user = pwd.getpwuid(stat_info.st_uid)[0]                            #Return the password database entry for the given numeric user ID.
+        user = pwd.getpwuid(stat_info.st_uid)[0]
         group = grp.getgrgid(stat_info.st_gid)[0]
-        mode = oct(stat.S_IMODE(stat_info.st_mode))                         #octal mode
-        mode_str = str(mode).replace('o', '')                               #replace o = octal mode for a ''
+        mode = oct(stat.S_IMODE(stat_info.st_mode))
+        mode_str = str(mode).replace('o', '')
         if len(mode_str) > 3:
-            mode = mode_str[-3:]                                            #get last 3 digits (just we need)
+            mode = mode_str[-3:]
         else:
             mode = mode_str
-        protection = get_filemode(stat_info.st_mode)        
-        if os.path.isdir(item):                                             #check if it is a directory or a file
+        protection = get_filemode(stat_info.st_mode)
+        if os.path.isdir(item):
             type = "directory"
         else:
             type = "file"
 
         return {'group': group, 'mode': mode, 'type': type, 'user': user, 'prot': protection}
-   
+
     except FileNotFoundError:
         return {'group': None, 'mode': None, 'type': None, 'user': None, 'prot': None}
     except OSError: #Permission denied
         pass
 
 
-
-# ---------------------------------------------------------------------------------------------------------------
-
-"""
-    Get a dictionary with all checkfile information from all files and directories located in a specific path
-    Parameters:
-        -path: Path to begin extract information.
-        -ignore_folders: Forders to be ignored
-    Return:
-        Dictonary with all check files corresponding to the analized path. It has the following format:
-        "/var/ossec/active-response":{
-                "group": "wazuh",
-                "mode": "0750",
-                "prot": "drwxr-x---",
-                "type": "directory",
-                "user": "root"
-        }, ...
-"""
-
 def get_current_items(path='/', ignore_folders=[]):
-    files_items_dict = {}                                                   
-    flag_no_file_detected = False                                           #flag to indicate if a file/folder was found
-    for (dirpath, dirnames, filenames) in os.walk(path, followlinks=False): #walk through the path
-        dirpath = remove_ending_bar_chars(dirpath)                          #remove ending "/" in case it has it
-        aux_dirpath = remove_initial_bar_chars(dirpath)                     #remove beginning "/" in case it has it
-        path_list = Counter(aux_dirpath.split('/'))                         #create a counter element (list of element separated by "/")
-        
-        for item_ignored in ignore_folders:                                 #walk through ignored folders
-            item_ignored = remove_initial_bar_chars(item_ignored)           #remove beginning "/" in case it has it
-            substract_folders = Counter(item_ignored.split('/')) - path_list#create a counter element (list of element separated by "/")
-                                                                            #and substract conter element "path_list", if result is an empty dict
-                                                                            #means that path match with the ignored element                   
-            if substract_folders == {}:                                     #if empty, break loop and analize next element 
+    """Get a dictionary with all checkfile information from all files and directories located in a specific path
+
+    Args:
+        path (string): Path to begin extract information.
+        ignore_folders (list): Forders lists to be ignored
+
+    Return:
+        dict: Dictonary with all check files corresponding to the analized path. It has the following format:
+            "/var/ossec/active-response":{
+                    "group": "wazuh",
+                    "mode": "0750",
+                    "prot": "drwxr-x---",
+                    "type": "directory",
+                    "user": "root"
+            }, ...
+    """
+    files_items_dict = {}
+    flag_no_file_detected = False
+    for (dirpath, dirnames, filenames) in os.walk(path, followlinks=False):
+        dirpath = remove_ending_bar_chars(dirpath)
+        aux_dirpath = remove_initial_bar_chars(dirpath)
+        path_list = Counter(aux_dirpath.split('/'))
+
+        for item_ignored in ignore_folders:
+            item_ignored = remove_initial_bar_chars(item_ignored)
+            substract_folders = Counter(item_ignored.split('/')) - path_list
+
+            if substract_folders == {}:
                 flag_no_file_detected = True
                 break
             else:
-                flag_no_file_detected = False                               #element was not found yet and enable watch for properties
+                flag_no_file_detected = False
         if not flag_no_file_detected:
-            files_items_dict[dirpath] = get_data_information(dirpath)       #store the directory element and they properties
-            for filename in filenames:                                      
-                file_path = f'{dirpath}/{filename}'                         
+            files_items_dict[dirpath] = get_data_information(dirpath)
+            for filename in filenames:
+                file_path = f'{dirpath}/{filename}'
                 if not file_path.endswith('.pyc') and not file_path in ignore_folders:
-                    files_items_dict[file_path] = get_data_information(file_path)#store files element and they properties
-        flag = False       
+                    files_items_dict[file_path] = get_data_information(file_path)
+        flag_no_file_detected = False
     return files_items_dict
 
 
-# ---------------------------------------------------------------------------------------------------------------
+def remove_initial_bar_chars(path):
+    """Remove "/" character in the beginning of a path in case exists
 
-"""
-    Remove "/" character in the beginning of a path in case exists
-    Parameters:
-        - path: path to be analized
+    Args:
+        path (string): Path to be analized
+
     Return:
-        - path without initial "/" character
+        string: Path without initial "/" character
 
     Example:
         - /home/user becomes home/user
-        
-"""
-def remove_initial_bar_chars(path):
+
+    """
     if path[0] == '/':
-        path=path[1:]    #[1:]remove 1st char    
+        path=path[1:]    #[1:]remove 1st char
     return path
 
-# ---------------------------------------------------------------------------------------------------------------
 
-"""
-    Remove "/" character in the the end of a path in case exists
-    Parameters:
-        - path: path to be analized
+def remove_ending_bar_chars(path):
+    """Remove "/" character in the the end of a path in case exists
+
+    Args:
+        path (string): Path to be analized
+
     Return:
-        - path without ending "/" character
+        string: Path without ending "/" character
 
     Example:
         - /home/user becomes home/user
-"""
-def remove_ending_bar_chars(path):    
+    """
     if path[len(path) - 1] == '/':
         path=path[:-1]    #[:-1]remove last char
     return path
 
-# ---------------------------------------------------------------------------------------------------------------
 
-"""
-    Class to be used in debug mode
-    Parameters:
-        - path: path to be analized
-        - os: operated systen
-        - ignore: path to be ignore
-        - no_show_ignore_list: flag to show/hide ignore paths
-"""
+def get_script_parameters():
+    """Generate option atributes to the entry point
 
-class Args:
-    def __init__(self, path, os, ignore, no_show_ignore_list=False) -> None:
-        self.path = path
-        self.os =  os
-        self.ignore = ignore
-        self.no_show_ignore_list = no_show_ignore_list
-        pass
+    Returns:
+        ArgumentParser: Atributes to the entry point
+    """
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-p", "--path", type=str, required=False, default='/',
+                            help="Path base to inspect files recursively")
+    arg_parser.add_argument("-i", "--ignore", type=str, default='/var/ossec', help="")
+    return arg_parser.parse_args()
 
-# ---------------------------------------------------------------------------------------------------------------
-
-"""
-    Entry point 
-
-    Execution example: 
-    
-    python3 check_files.py -p '/home/fede/Downloads' -o 'linux' -i '/var/ossec,/home/fede/Downloads/wazuh.json'
-    
-"""
-
-# ---------------------------------------------------------------------------------------------------------------
-
-DEBUG_MODE = False
 
 if __name__ == "__main__":
+    """Main function
 
-    if not DEBUG_MODE:
-        arg_parser = argparse.ArgumentParser()
-        arg_parser.add_argument("-p", "--path", type=str, required=True, help="/home/user")
-        arg_parser.add_argument("-o", "--os", type=str, required=True,  choices=['linux', 'windows', 'redhat', 'debian'], help="Operating system or host distribution")
-        arg_parser.add_argument("-i", "--ignore", type=str, help="Ignore path: /var/ossec,/home")
-        arg_parser.add_argument("-n", "--no_show_ignore_list", action="store_true", help="Show ignore list")
-        args = arg_parser.parse_args()
-    else:
-        args = args = Args('/home/fede/Videos', 'linux', '/var/ossec,/home/fede/Videos/Wazuh-meets')
-    
+    Execution example:
+        python3 check_files.py -p '/home/fede/Downloads' -i '/var/ossec,/home/fede/Downloads/wazuh.json'
+    """
+    argument = get_script_parameters()
+
     try:
-        print(str(args))
         print("Checking files...")
 
-        template_file = f'{args.os}_files.json'                             #create output json file 
-        original_path = args.path                                           #get current path to be analized
-        no_show_ignore_list = args.no_show_ignore_list                      #flag that indicate if it will show ignored folder
+        template_file = f'check_files.json'
+        original_path = argument.path
+        ignore_folders = argument.ignore.split(',') if argument.ignore else []
+        current_items = get_current_items(original_path, ignore_folders)
 
-        if args.ignore:                                                     #get ignored folder as a list
-            ignore_folders = args.ignore.split(',')
-        
-        current_items = get_current_items(original_path, ignore_folders)    #get list of dict with the files information
-
-       
         dictArray = {}
         element_list = []
         description_list = []
-        i = 0       
-        for name in sorted(current_items):                                  #order information to be stored in a json file
-            id = i            
+        i = 0
+        for name in sorted(current_items):
+            id = i
             try:
                 group =  current_items[name]['group']
             except TypeError:
@@ -299,18 +270,15 @@ if __name__ == "__main__":
             element_list.append({
                 "id": i,
                 "name": name,
-                "description" : description_list[i]                
-            })            
+                "description" : description_list[i]
+            })
             i = i + 1
-
         dictArray = {"data" : element_list}
-        with open(template_file, 'w') as convert_file:                      #create a json file
-            convert_file.write(json.dumps(dictArray))
-        
-        if len(ignore_folders) > 0 and not no_show_ignore_list:             #show ignored folders/files
+        with open(template_file, 'w') as convert_file:
+            convert_file.write(json.dumps(dictArray, indent=4))
+        if len(ignore_folders) > 0:
             print("\nIgnored:")
             print('\n'.join(sorted(set(ignore_folders))))
-       
         print("\nCongrats!.")
 
     except Exception as e:
